8832faaf63e6fbaaeb2d50befa53d86547c31042
Eric-cv <zhangqi_@pku.edu.cn>
2020-05-11 19:57:03 +0800

    test how git works

---
+++ test_git.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# @Time     :2020/5/11 0011 19:51
+# @Author   :Eric
+# @Email    :zhangqi_@pku.edu.cn
+# @File     :test_git.py
+# @Software :PyCharm
+
+
+# nothing to say ,just test git how to work
